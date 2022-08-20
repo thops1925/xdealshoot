a3656b3067fcca939b83bfa0f0ed8ed6353b1408
ThopsD <74445064+thops1925@users.noreply.github.com>
2022-08-21 00:30:27 +0800

    fix bug ready to deploy

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,21 +7,11 @@ function Card({ slideOpen, id, title, photo, setItem }) {
 
     return (
         <div key={id} onClick={slideOpen} className="relative">
-            <div
-                onClick={() => itemResult(photo)}
-                className="absolute inset-0 z-10 flex"
-            >
+            <div onClick={() => itemResult(photo)} className="absolute inset-0 z-10 flex">
                 <div className="absolute inset-0 opacity-0 bg-black hover:opacity-0 "></div>
-                <div className="mx-auto text-white z-10 self-center uppercase  text-lg">
-                    {title}
-                </div>
+                <div className="mx-auto text-white z-10 self-center uppercase  text-lg">{title}</div>
             </div>
-            <img
-                src={photo[2]}
-                onClick={() => itemResult(photo)}
-                alt="icons"
-                className="w-full h-98 rounded overflow-hidden object-cover"
-            />
+            <img src={photo[2]} onClick={() => itemResult(photo)} alt="icons" className="w-full h-98 rounded overflow-hidden object-cover" />
         </div>
 
     );
